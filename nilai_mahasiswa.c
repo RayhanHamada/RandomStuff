75ee72a018039b3d12f5f99d49c5ebdd29119064
Mocchapine <hamadadesk1999@gmail.com>
2019-11-18 19:44:50 +0700

    add nilai_mahasiswa.c

---
+++ nilai_mahasiswa.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main() {
+    int daftarBanyakNilai[5] = {0, 0, 0, 0, 0}; 
+    for (int i = 0;i < 10; i+= 1){
+        char c;
+
+        printf("Nilai Mahasiswa %d : ", i + 1);
+        scanf("\n%c", &c);
+
+        switch(c) {
+            case 'A':
+                daftarBanyakNilai[0]++;
+                break;
+            case 'B':
+                daftarBanyakNilai[1]++;
+                break;
+            case 'C':
+                daftarBanyakNilai[2]++;
+                break;
+            case 'D':
+                daftarBanyakNilai[3]++;
+                break;
+            case 'E':
+                daftarBanyakNilai[4]++;
+            default:
+                break;
+        }
+    }
+
+    for (int j = 0; j < 5; j++) {
+        printf("Total Nilai %c : %d\n", (char)65 + j, daftarBanyakNilai[j]);
+    }
+}
